d812d8fb2f4f4fb7e5f42d5f7986328993261f14
Bergeron, Stephen <Stephen.Bergeron@kronos.com>
2017-06-21 15:44:19 -0400

    Work on loganalyzer utility. Keep a temporary copy of the log files. 	modified:   loganalyzer/project.clj 	new file:   loganalyzer/resources/access.log 	new file:   loganalyzer/resources/catalina.out 	new file:   loganalyzer/resources/wfc.log 	modified:   loganalyzer/test/loganalyzer/core_test.clj

--- loganalyzer/project.clj
+++ loganalyzer/project.clj
@@ -1,6 +1,10 @@
 (defproject loganalyzer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Simple log analyzer with basic classification ability."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]]
+  :plugins [[lein-auto "0.1.2"]
+            [lein-ancient "0.6.8"]]
+  )
--- loganalyzer/test/loganalyzer/core_test.clj
+++ loganalyzer/test/loganalyzer/core_test.clj
@@ -1,7 +1,30 @@
 (ns loganalyzer.core-test
   (:require [clojure.test :refer :all]
-            [loganalyzer.core :refer :all]))
+            [loganalyzer.core :refer :all]
+            [clojure.data.json :as json]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def
+  simulation-dir
+  "/home/stn/cache/cxseed/scheduling.schedule.MultiReadDesktopSimulation.COLD/cxseed-a-back/")
+
+(def logs
+  {:wfc      (str simulation-dir "wfc.log")
+   :catalina (str simulation-dir "catalina.out")
+   :access   (str simulation-dir "access.log")})
+
+(deftest logs-exist-test
+  (testing "wfc.log file exist"
+    (is (.exists (clojure.java.io/file (logs :wfc))))
+    (is (.exists (clojure.java.io/file (logs :catalina))))
+    (is (.exists (clojure.java.io/file (logs :access))))))
+
+
+
+
+;; (def wfc-json
+;;   (json/read-str (slurp (logs :wfc)) :key-fn keyword))
+
+
+;; (deftest wfc-log-json-test
+;;   (testing "wfc.log file can be read in json format"
+;;     (is (= "" wfc-json))))
