05ef99ffa8f8b1fed3338ce491be9750d6efb5d5
Bergeron, Stephen <Stephen.Bergeron@kronos.com>
2017-06-22 09:56:02 -0400

    Add more test 	modified:   test/loganalyzer/core_test.clj

--- loganalyzer/test/loganalyzer/core_test.clj
+++ loganalyzer/test/loganalyzer/core_test.clj
@@ -5,7 +5,7 @@
 
 (def
   simulation-dir
-  "/home/stn/cache/cxseed/scheduling.schedule.MultiReadDesktopSimulation.COLD/cxseed-a-back/")
+  "resources/")
 
 (def logs
   {:wfc      (str simulation-dir "wfc.log")
@@ -18,13 +18,15 @@
     (is (.exists (clojure.java.io/file (logs :catalina))))
     (is (.exists (clojure.java.io/file (logs :access))))))
 
+;; (deftest log-wfc-is-parsable
+;;   (testing "wfc.log is parsable"
+;;     (let [content (json/read-str (slurp (logs :wfc)) :key-fn keyword)]
+;;       (is (= "" content))))
 
-
-
-;; (def wfc-json
-;;   (json/read-str (slurp (logs :wfc)) :key-fn keyword))
-
-
-;; (deftest wfc-log-json-test
-;;   (testing "wfc.log file can be read in json format"
-;;     (is (= "" wfc-json))))
+(deftest log-access-is-parsable
+  (testing "access.log from nginx is parsable"
+    (let [content  (-> :access (logs) (slurp) (clojure.string/split #"\n"))
+          presplit (map (fn[x] (clojure.string/split x #"\"")) content)]
+      (is (=
+           "10.129.62.6 - - 21/Jun/2017:19:02:35 +0000 \"POST /wfc/XmlService?tenantId=healthcare HTTP/1.1\" 499 0"
+           (first content))))))
