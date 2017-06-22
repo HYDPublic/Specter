dd181724e87eb8e7a0e69fc051891d03c750b72f
Bergeron, Stephen <Stephen.Bergeron@kronos.com>
2017-06-22 14:17:51 -0400

    On Duty: KAIZEN 	modified:   test/loganalyzer/core_test.clj

--- loganalyzer/test/loganalyzer/core_test.clj
+++ loganalyzer/test/loganalyzer/core_test.clj
@@ -29,17 +29,38 @@
 ;;     (let [content (json/read-str (slurp (logs :wfc)) :key-fn keyword)]
 ;;       (is (= "" content))))
 
+;remote_addr dummy remote_user $time_local time_local2 "$request" $status $body_bytes_sent $request_time
+
 (deftest log-access-is-parsable
   (testing "access.log from nginx is parsable"
     (let [content  (-> :access (logs) (slurp) (clojure.string/split #"\n"))
           presplit (map (fn[x]
-                          (let [p1 (clojure.string/split x #"\"")]
-                            p1))
+                          (let [p      (clojure.string/split x #"\"")
+                                p1     (clojure.string/split (first p) #"\s")
+                                p2     (clojure.string/split (second p) #"\s")
+                                p3     (clojure.string/split (nth p 2) #"\s")
+                                remote-addr (nth p1 0)
+                                tstamp (str (nth p1 3) " " (nth p1 4))
+                                verb    (nth p2 0)
+                                request (nth p2 1)
+                                status          (nth p3 1)
+                                body-byte-sent  (nth p3 2)
+                                ]
+                            {:ts             tstamp
+                             :remote-addr    remote-addr
+                             :verb           verb
+                             :request        request
+                             :http-status    status
+                             :body-byte-sent body-byte-sent}))
                         content)]
       (is (= (samples :line1) (nth content 1)))
       (is (= (samples :line8) (nth content 8)))
       (is (=
-           ["10.129.62.6 - - 21/Jun/2017:19:02:35 +0000 "
-            "POST /wfc/XmlService?tenantId=healthcare HTTP/1.1"
-            " 499 0"]
+           {:ts          "21/Jun/2017:19:02:35 +0000"
+            :remote-addr "10.129.62.6"
+            :request     "/wfc/XmlService?tenantId=healthcare"
+            :verb        "POST"
+            :http-status    "499"
+            :body-byte-sent "0"
+            }
            (nth presplit 1))))))
