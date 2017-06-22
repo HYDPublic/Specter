368f7b4ff8752d5943c19f3fbc2f8c1ed5fe894d
Bergeron, Stephen <Stephen.Bergeron@kronos.com>
2017-06-22 11:51:00 -0400

    Refactoring - test are 100%

--- loganalyzer/test/loganalyzer/core_test.clj
+++ loganalyzer/test/loganalyzer/core_test.clj
@@ -18,6 +18,12 @@
     (is (.exists (clojure.java.io/file (logs :catalina))))
     (is (.exists (clojure.java.io/file (logs :access))))))
 
+
+(def samples {:line1
+              "10.129.62.6 - - 21/Jun/2017:19:02:35 +0000 \"POST /wfc/XmlService?tenantId=healthcare HTTP/1.1\" 499 0"
+              :line8
+              "10.129.62.6 - - 21/Jun/2017:19:28:56 +0000 \"POST /wfc/restcall/v1/scheduling/schedule/multi_read HTTP/1.1\" 200 933246"})
+
 ;; (deftest log-wfc-is-parsable
 ;;   (testing "wfc.log is parsable"
 ;;     (let [content (json/read-str (slurp (logs :wfc)) :key-fn keyword)]
@@ -26,7 +32,14 @@
 (deftest log-access-is-parsable
   (testing "access.log from nginx is parsable"
     (let [content  (-> :access (logs) (slurp) (clojure.string/split #"\n"))
-          presplit (map (fn[x] (clojure.string/split x #"\"")) content)]
+          presplit (map (fn[x]
+                          (let [p1 (clojure.string/split x #"\"")]
+                            p1))
+                        content)]
+      (is (= (samples :line1) (nth content 1)))
+      (is (= (samples :line8) (nth content 8)))
       (is (=
-           "10.129.62.6 - - 21/Jun/2017:19:02:35 +0000 \"POST /wfc/XmlService?tenantId=healthcare HTTP/1.1\" 499 0"
-           (first content))))))
+           ["10.129.62.6 - - 21/Jun/2017:19:02:35 +0000 "
+            "POST /wfc/XmlService?tenantId=healthcare HTTP/1.1"
+            " 499 0"]
+           (nth presplit 1))))))
